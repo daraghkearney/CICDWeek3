f7eaf78a301920064737554e1b9172512bae5d75
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-10-01 15:34:20 +0100

    Added endpoint: POST

--- src/main/java/ie/atu/week3cicd1/ProductController.java
+++ src/main/java/ie/atu/week3cicd1/ProductController.java
@@ -1,22 +1,30 @@
 package ie.atu.week3cicd1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/product")
 @RestController
 public class ProductController {
+    List<Product> myList = new ArrayList<>();
 
     @GetMapping("/hello")
     public String hello()
     {
         return "hello";
     }
-    @GetMapping("/getproduct")
-    public Product getProduct()
+    @GetMapping("/getproducts")
+    public List<Product> getProducts()
     {
         Product myProduct = new Product("Tv", 499);
+        return myList;
+    }
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product myProduct)
+    {
+        myList.add(myProduct);
         return myProduct;
     }
 }
