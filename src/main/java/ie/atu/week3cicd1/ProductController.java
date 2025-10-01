6223d541b24ea9586729170b2b596bf49dbc3597
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-10-01 14:52:23 +0100

    Added feature getProduct

---
+++ src/main/java/ie/atu/week3cicd1/ProductController.java
@@ -0,0 +1,22 @@
+package ie.atu.week3cicd1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/product")
+@RestController
+public class ProductController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+    @GetMapping("/getproduct")
+    public Product getProduct()
+    {
+        Product myProduct = new Product("Tv", 499);
+        return myProduct;
+    }
+}
